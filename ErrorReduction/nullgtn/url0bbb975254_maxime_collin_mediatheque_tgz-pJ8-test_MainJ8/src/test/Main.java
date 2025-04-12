86436c33647d7ceec16e857aab7ea291657a1ccf
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-12 16:10:43 -0700

    add annotator wpi nullgtn inference output

--- ErrorReduction/nullgtn/url0bbb975254_maxime_collin_mediatheque_tgz-pJ8-test_MainJ8/src/test/Main.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class Main {
-
-    public static void main(@javax.annotation.Nullable String[] args) {
-        Bdd.main(null);
-        ControleurTest.main();
-    }
-}
--- ErrorReduction/nullgtn/url0d38a10ee7_diegopliebana_GEBT_tgz-pJ8-ch_idsia_scenarios_test_EvolveWithChangingSeedsJ8/src/ch/idsia/mario/engine/sprites/SpriteContext.java
+++
@@ -1,8 +0,0 @@
-package ch.idsia.mario.engine.sprites;
-
-public interface SpriteContext {
-
-    public void addSprite(@javax.annotation.Nullable Sprite sprite);
-
-    public void removeSprite(@javax.annotation.Nullable Sprite sprite);
-}
