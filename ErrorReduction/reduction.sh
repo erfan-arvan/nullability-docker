8593763d3cd41aa8e780a0a8117bc4a6505631a6
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-12 18:50:03 -0700

    update

---
+++ ErrorReduction/reduction.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# if fresh passed rerun nullaway.py
+if [ "$1" == "--fresh" ]; then
+    python3 scripts/run_nullaway.py
+    python3 scripts/run_CFnullness.py
+fi
+
+python3 count_error_reduction.py
